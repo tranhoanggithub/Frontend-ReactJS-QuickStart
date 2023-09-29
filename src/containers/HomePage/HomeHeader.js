f1cab847f2dadd9b4eadbb82cd0716091e936a55
tranhoang <mailtranhoang@gmail.com>
2023-09-29 23:34:45 +0700

    Su dung redux de chuyen doi ngon ngu trong React App

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,11 +3,19 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.svg'
 import { FormattedMessage } from 'react-intl';
+import {LANGUAGES} from "../../utils";
+
+import {changeLanguageApp} from "../../store/actions"
 
 class HomeHeader extends Component {
 
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
+
     render() {
-        console.log('check props', this.props)
+        let language = this.props.language;
+        console.log('check props', language)
 
         return (
             <React.Fragment>
@@ -41,8 +49,8 @@ class HomeHeader extends Component {
                         <div className="support"><i className="fas fa-question-circle"></i>
                             <FormattedMessage id = "homeheader.support"/>
                         </div>
-                        <div className="language-vi">VN</div>
-                        <div className="language-en">EN</div>
+                        <div className={language === LANGUAGES.VI ? "language-vi active":"language-vi"}><span onClick={()=>this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                        <div className={language === LANGUAGES.EN ? "language-en active":"language-en"}><span onClick={()=>this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                     </div>
                 </div>
                 <div className="home-header-banner">
@@ -94,12 +102,13 @@ class HomeHeader extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        langguage: state.app.language,
+        language: state.app.language,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)) 
     };
 };
 
