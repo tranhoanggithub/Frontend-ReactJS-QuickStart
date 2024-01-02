6cbf3ea67333cc89d63e8ab496e89c71c0b42adf
tranhoang <mailtranhoang@gmail.com>
2024-01-02 12:23:18 +0700

    #54 Phep mau khien redux la lua chon Number 1 voi React App

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,4 +1,4 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import appReducer from "./appReducer";
@@ -19,11 +19,18 @@ const persistCommonConfig = {
 const userPersistConfig = {
     ...persistCommonConfig,
     key: 'user',
-    whitelist: ['isLoggedIn', 'userInfo' , 'language']
+    whitelist: ['isLoggedIn', 'userInfo']
 };
+
+const appPersistConfig = {
+    ...persistCommonConfig,
+    key: 'app',
+    whitelist: ['language']
+
+}
 
 export default (history) => combineReducers({
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
-    app: appReducer
+    app: persistReducer(appPersistConfig, appReducer)
 })
